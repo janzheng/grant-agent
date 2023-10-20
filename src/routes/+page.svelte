a594faa1a40df500c4d15000f9cb0ea80de10f5c
Jan Zheng <janzheng@Jans-CrazyBook-Pro.local>
2023-10-21 07:17:05 +1100

    brr

---
+++ src/routes/+page.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+  import { Textarea } from "$lib/components/ui/textarea";
+  import { Button } from "$lib/components/ui/button";
+  import * as Card from "$lib/components/ui/card";
+  import * as Select from "$lib/components/ui/select";
+  import { Input } from "$lib/components/ui/input";
+  import { Label } from "$lib/components/ui/label";
+
+  const frameworks = [
+    {
+      value: "sveltekit",
+      label: "SvelteKit"
+    },
+    {
+      value: "next",
+      label: "Next.js"
+    },
+    {
+      value: "astro",
+      label: "Astro"
+    },
+    {
+      value: "nuxt",
+      label: "Nuxt.js"
+    }
+  ];
+
+
+  import { z } from "zod";
+  export const formSchema = z.object({
+    username: z.string().min(2).max(50)
+  });
+  import * as Form from "$lib/components/ui/form";
+  import { superValidate } from "sveltekit-superforms/server";
+  export const load = () => {
+    return {
+      form: superValidate(formSchema)
+    };
+  };
+  export let form;
+
+</script>
+
+
+<div class="container max-w-3xl">
+
+  <div class="mb-4">
+    <Textarea placeholder="Type your message here." />
+  </div>
+  <div class="mb-4">
+    <Form.Root method="POST" {form} schema={formSchema} let:config>
+      <Form.Field {config} name="username">
+        <Form.Item>
+          <Form.Label>Username</Form.Label>
+          <Form.Input />
+          <Form.Description>This is your public display name.</Form.Description>
+          <Form.Validation />
+        </Form.Item>
+      </Form.Field>
+      <Form.Button>Submit</Form.Button>
+    </Form.Root>
+  </div>
+  <div class="mb-4">
+    <Card.Root class="w-[350px]">
+      <Card.Header>
+        <Card.Title>Create project</Card.Title>
+        <Card.Description>Deploy your new project in one-click.</Card.Description>
+      </Card.Header>
+      <Card.Content>
+        <form>
+          <div class="grid w-full items-center gap-4">
+            <div class="flex flex-col space-y-1.5">
+              <Label for="name">Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+            <div class="flex flex-col space-y-1.5">
+              <Label for="framework">Framework</Label>
+              <Select.Root>
+                <Select.Trigger id="framework">
+                  <Select.Value placeholder="Select" />
+                </Select.Trigger>
+                <Select.Content>
+                  {#each frameworks as framework}
+                    <Select.Item value={framework.value} label={framework.label}
+                      >{framework.label}</Select.Item
+                    >
+                  {/each}
+                </Select.Content>
+              </Select.Root>
+            </div>
+          </div>
+        </form>
+      </Card.Content>
+      <Card.Footer class="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </Card.Footer>
+    </Card.Root>
+  </div>
+</div>
